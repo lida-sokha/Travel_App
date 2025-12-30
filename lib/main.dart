aae8c1ecf7fd4aeb0be7ada6cfa372845273dbaa
lida-sokha <sokhalida1234@gmail.com>
2025-12-30 23:51:32 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Directionality(
+      textDirection: TextDirection.ltr,
+      child: Center(child: Text('Hello')),
+    );
+  }
+}
